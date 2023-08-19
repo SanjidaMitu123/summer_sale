61eff8aa1f40a90c0cf7149910a50905fab076a6
SanjidaMitu123 <sanjidamitu456@gmail.com>
2023-08-20 04:55:57 +0600

    modal and js without discount tatal added

--- summersale.js
+++ summersale.js
@@ -1,1 +1,17 @@
-console.log('ok');
+
+
+let total = 0 ;
+function clickwork(target){
+    const selectedItemContainer = document.getElementById("selected-item");
+const itemname= target.childNodes[3].childNodes[3].innerText;
+const li = document.createElement("li");
+    li.innerText = itemname ;
+    selectedItemContainer.appendChild(li);
+
+    const price1 = target.childNodes[3].childNodes[5].childNodes[0].innerText;
+    console.log(price1)
+    total  = parseInt(total )+ parseInt (price1);
+console.log (total);
+    document.getElementById("withoutdistotal").innerText = total ;
+
+}
